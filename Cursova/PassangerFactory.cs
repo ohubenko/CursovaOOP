75f64f2818ddc0c64d38973244d69f4f98df1959
Oleksandr Hubenko <sasha0420011@gmail.com>
2021-05-30 16:52:40 +0300

    Extract Passanger factory

---
+++ Cursova/PassangerFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace Cursova
+{
+    public static class PassangerFactory
+    {
+        public static Passanger servicePassanger(Human human, AirlineClass @class)
+        {
+            Ticket ticket = new Ticket(@class);
+            return @class switch
+            {
+                AirlineClass.FIST => new FirstclassPassanger(human, ticket),
+                AirlineClass.SECOND => new SecondclassPassanger(human, ticket),
+                _ => new SecondclassPassanger(human, ticket)
+            };
+        }
+    }
+}
--- Cursova/ServiceDeskModel/AbstractServiceDesk.cs
+++ Cursova/ServiceDeskModel/AbstractServiceDesk.cs
@@ -48,19 +48,5 @@ namespace Cursova
                 _ => AirlineClass.SECOND
             };
         }
-
-        private static class PassangerFactory
-        {
-            public static Passanger servicePassanger(Human human, AirlineClass @class)
-            {
-                Ticket ticket = new Ticket(@class);
-                return @class switch
-                {
-                    AirlineClass.FIST => new FirstclassPassanger(human, ticket),
-                    AirlineClass.SECOND => new SecondclassPassanger(human, ticket),
-                    _ => new SecondclassPassanger(human, ticket)
-                };
-            }
-        }
     }
 }
